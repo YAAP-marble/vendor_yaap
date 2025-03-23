e61267e0e4d6ffb58f7c09f8b492d6f00a823f63
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-23 13:51:43 -0700

    vendorsetup: Update host metadata overrides for 15 QPR2
    * Matches pattern used in Pixels in 15.0 QPR2 factory images: https://dumps.tadiphone.dev/dumps/google/bluejay/-/blob/bluejay-user-15-BP1A.250405.005-13150826-release-keys/system/system/build.prop
    
    Reference:
    https://grapheneos.social/@GrapheneOS/113870255397154703
    https://github.com/GrapheneOS/platform_build/blob/15-qpr2/envsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Shebang is intentionally missing - do not run as a script
 
 # Override host metadata to make builds more reproducible and avoid leaking info
-[ -z "${BUILD_USERNAME}" ] && export BUILD_USERNAME=nobody
-[ -z "${BUILD_HOSTNAME}" ] && export BUILD_HOSTNAME=android-build
+[ -z "${BUILD_USERNAME}" ] && export BUILD_USERNAME=android-build
+[ -z "${BUILD_HOSTNAME}" ] && export BUILD_HOSTNAME=r-0123456789abcdef-acab
