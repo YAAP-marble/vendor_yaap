d799e93042c9240f49ffeef85b4b5365e2634ba1
John Galt <johngaltfirstrun@gmail.com>
2025-04-01 11:50:10 -0400

    config/common: do not include virtualization support by default

--- config/common.mk
+++ config/common.mk
@@ -198,9 +198,3 @@ $(call inherit-product, vendor/themes/common.mk)
 
 # Sepolicy
 $(call inherit-product, vendor/yaap/config/sepolicy.mk)
-
-# Virtualization
-$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.hypervisor.vm.supported=1 \
-    ro.boot.hypervisor.protected_vm.supported=1
